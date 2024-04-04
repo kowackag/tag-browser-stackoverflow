2d079168eed63151d8d422021f48fa64f6c4e63f
mkowack <kowackag@gmail.com>
2024-04-04 20:52:52 +0200

    Fix link in pagination

--- src/ui/molecules/Pagination.tsx
+++ src/ui/molecules/Pagination.tsx
@@ -1,4 +1,59 @@
+// import { type Route } from "next";
+
+// import { ActiveLink } from "@/ui/atoms/ActiveLink";
+// import { usePagination } from "@/utils/hooks/usePagination";
+
+// export const Pagination = ({
+// 	currentPage,
+// 	totalTags,
+// 	pageSize,
+// }: {
+// 	currentPage: number;
+// 	totalTags: number;
+// 	pageSize: number;
+// }) => {
+// 	const paginationRange = usePagination({
+// 		currentPage: currentPage,
+// 		totalCount: totalTags,
+// 		pageSize: pageSize,
+// 	});
+
+// 	if (!paginationRange) return;
+
+// 	return (
+// 		<nav
+// 			className="mt-8 flex items-center justify-between border-t border-zinc-100 px-4 align-middle sm:px-0"
+// 			aria-label="pagination"
+// 		>
+// 			<ul className="mx-auto hidden md:-mt-px md:flex">
+// 				{paginationRange.map((pageNumber, ind) =>
+// 					pageNumber === "..." ? (
+// 						<span key={`dot-${ind}`} className="inline-block py-4">
+// 							...
+// 						</span>
+// 					) : (
+// 						<li key={pageNumber}>
+// 							<ActiveLink
+// 								href={`/tags/${pageNumber}` as Route}
+// 								className="inline-flex items-center border-t-4 border-transparent px-6 py-4 text-sm  hover:border-sky-600"
+// 								activeClassName="text-sky-700 font-semibold"
+// 								exact={true}
+// 								aria-label={`pagination ${pageNumber}`}
+// 							>
+// 								{pageNumber}
+// 							</ActiveLink>
+// 						</li>
+// 					),
+// 				)}
+// 			</ul>
+// 		</nav>
+// 	);
+// };
+
+"use client";
+
 import { type Route } from "next";
+import { useSearchParams } from "next/navigation";
 
 import { ActiveLink } from "@/ui/atoms/ActiveLink";
 
@@ -18,6 +73,9 @@ export const Pagination = ({
 		totalCount: totalTags,
 		pageSize: pageSize,
 	});
+	const searchParams = useSearchParams();
+
+	const params = new URLSearchParams(searchParams);
 
 	if (!paginationRange) return;
 
@@ -26,7 +84,7 @@ export const Pagination = ({
 			className="mt-8 flex items-center justify-between border-t border-zinc-100 px-4 align-middle sm:px-0"
 			aria-label="pagination"
 		>
-			<ul className="mx-auto hidden md:-mt-px md:flex">
+			<ul className="mx-auto -mt-px flex">
 				{paginationRange.map((pageNumber, ind) =>
 					pageNumber === "..." ? (
 						<span key={`dot-${ind}`} className="inline-block py-4">
@@ -35,10 +93,10 @@ export const Pagination = ({
 					) : (
 						<li key={pageNumber}>
 							<ActiveLink
-								href={`/tags/${pageNumber}` as Route}
+								href={`/tags/${pageNumber}?${params}` as Route}
 								className="inline-flex items-center border-t-4 border-transparent px-6 py-4 text-sm  hover:border-sky-600"
 								activeClassName="text-sky-700 font-semibold"
-								exact={true}
+								exact={false}
 								aria-label={`pagination ${pageNumber}`}
 							>
 								{pageNumber}
